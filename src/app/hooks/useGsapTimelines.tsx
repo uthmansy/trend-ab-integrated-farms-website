e615ae88cc9015546cea6081c8ca02626591f428
uthmansy <uthmansy@gmail.com>
2024-11-16 19:55:39 +0100

    Deploy one

--- src/app/hooks/useGsapTimelines.tsx
+++ src/app/hooks/useGsapTimelines.tsx
@@ -14,17 +14,15 @@ function useGsapTimelines(): Return {
 
   useEffect(() => {
     // Initialize Locomotive Scroll
-    loco.current = new LocomotiveScroll({
-      el: scrollRef.current || undefined,
-      smooth: true,
-      multiplier: 0.4, // Adjust scroll speed
-      tablet: {
-        breakpoint: 0,
-      },
-    });
-
+    // loco.current = new LocomotiveScroll({
+    //   el: scrollRef.current || undefined,
+    //   smooth: true,
+    //   multiplier: 0.4, // Adjust scroll speed
+    //   tablet: {
+    //     breakpoint: 0,
+    //   },
+    // });
     // loco.current.on("scroll", ScrollTrigger.update);
-
     // Tell ScrollTrigger to use Locomotive Scroll for scroll events
     // ScrollTrigger.scrollerProxy(scrollRef.current, {
     //   scrollTop(value) {
@@ -43,28 +41,23 @@ function useGsapTimelines(): Return {
     //       height: window.innerHeight,
     //     };
     //   },
-
     //   pinType:
     //     //@ts-ignore
     //     scrollRef.current && scrollRef.current.style.transform
     //       ? "transform"
     //       : "fixed",
     // });
-
     // heroTimeline(scrollRef);
     // aboutTimelines(scrollRef);
-
     // Refresh ScrollTrigger and Locomotive Scroll
     // ScrollTrigger.addEventListener("refresh", () => {
     //   loco.current.update();
     // });
-
     // ScrollTrigger.refresh();
-
-    return () => {
-      loco.current.destroy();
-      // ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-    };
+    // return () => {
+    //   loco.current.destroy();
+    // ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    // };
   }, []);
   return { scrollRef };
 }
