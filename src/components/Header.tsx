e6efbf93c1d5718c2ef6df9c0cb87521a9b56632
uthmansy <uthmansy@gmail.com>
2024-11-17 09:43:05 +0100

    proper locomotive scroll setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,55 @@
 
 import { HERO } from "@/constants/IMAGES";
 import TopNav from "./TopNav";
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 import MobileMenu from "./MobileMenu";
 import Hero from "./Hero";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const headerRef = useRef(null);
+  const bgRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".site-header",
+        scrub: true,
+        start: "top 0",
+        end: "bottom 50%",
+      },
+    });
+
+    tl.from(bgRef.current, { y: -100 });
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <header className="relative site-header w-full h-screen text-white overflow-hidden">
+    <header
+      ref={headerRef}
+      className="relative site-header w-full h-screen text-white"
+    >
       {/* Background Image as an Actual Element */}
       <div
-        data-scroll
-        data-scroll-speed="-0.3" // Adjusted speed for the parallax effect
+        ref={bgRef}
+        // data-scroll
+        // data-scroll-speed="-0.3" // Adjusted speed for the parallax effect
         className="absolute inset-0 bg-cover bg-center z-0"
-        style={{
-          backgroundImage: `url('${HERO.src}')`,
-          filter: "brightness(30%)",
-        }}
-      ></div>
+      >
+        <img
+          className="w-full object-cover"
+          src={HERO.src}
+          style={{
+            filter: "brightness(30%)",
+            height: "120%",
+          }}
+        />
+      </div>
 
       <TopNav isOpen={isOpen} toggleMenu={toggleMenu} />
       <MobileMenu isOpen={isOpen} toggleMenu={toggleMenu} />
